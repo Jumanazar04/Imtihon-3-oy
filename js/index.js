624f0d72d1062a249f8eb29bf31b090ef59467ec
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-03-15 17:45:43 +0500

    changed3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector('.menu__btn');
-const menuCard = document.querySelector('.menu__card');
+const menuCard = document.querySelector('.menu__card'),
+    form1 = document.querySelector('.form1'),
+    form1Bnt = document.querySelector('.form1__btn'),
+    input = document.querySelector('.input');
 
 
 btn.addEventListener('click', function (){
     menuCard.classList.toggle('active')
 })
+
